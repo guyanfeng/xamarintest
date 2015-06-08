6f71fabd623c76505b88982329e6f0509d1e75cf
guyanfeng <flysky82@gmail.com>
2015-06-08 11:51:16 +0800

    add styles page

---
+++ Test/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Test
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            if (Properties.ContainsKey("ResultText"))
+            {
+                ResultText = Convert.ToString(Properties["ResultText"]);
+            }
+
+            MainPage = new DynamicStylePage();
+        }
+
+        public string ResultText
+        {
+            get;
+            set;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+            Properties["ResultText"] = ResultText;
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Test/DynamicStylePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Test
+{
+    public partial class DynamicStylePage : ContentPage
+    {
+        public DynamicStylePage()
+        {
+            InitializeComponent();
+        }
+
+        void OnBtn1Clicked(object sender, EventArgs e)
+        {
+            Resources["buttonStyle"] = Resources["buttonStyle1"];
+        }
+
+        void OnBtn2Clicked(object sender, EventArgs e)
+        {
+            Resources["buttonStyle"] = Resources["buttonStyle2"];
+        }
+
+        void OnBtn3Clicked(object sender, EventArgs e)
+        {
+            Resources["buttonStyle"] = Resources["buttonStyle3"];
+        }
+    }
+}
