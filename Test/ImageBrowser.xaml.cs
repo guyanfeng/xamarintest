ccba5f379bf989183cb0d4b52c83cd78f6200b69
guyanfeng <flysky82@gmail.com>
2015-06-16 10:44:42 +0800

    add webitmap， resourcelist， imagebrowser

---
+++ Test/ImageBrowser.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Test
+{
+    public partial class ImageBrowser : ContentPage
+    {
+        public ImageBrowser()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
